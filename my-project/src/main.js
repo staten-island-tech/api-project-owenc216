c37cd37c853209c0b7a8c10d8516e9ef8bc69051
owenc216 <owen216413@gmail.com>
2025-12-19 14:00:06 -0500

    noooooooooo why are u noooooooooooooooootttttttttttttttttt heeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrreeeeeeeeeeeeeeeeee

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import "./style.css";
+const apikey = "bAWulOp5JBsdCZvLITJBAiQRVq5sRrGf";
+async function getDatar(poke) {
+  try {
+    //get data from API
+    const response = await fetch(`/api/v1/latest?api_key=${apikey}`);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //converts response into json we can use
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+console.log("ochen da");
--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,4 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.currencyscoop.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
