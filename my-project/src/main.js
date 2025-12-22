920f692ad4eb5f51887d26f7dc58359880f945f2
owenc216 <owen216413@gmail.com>
2025-12-22 14:01:59 -0500

    SCFSA

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 const apikey = "bAWulOp5JBsdCZvLITJBAiQRVq5sRrGf";
-async function getDatar(poke) {
+async function convertCurrency(from, to, amount) {
   try {
     //get data from API
     const response = await fetch(`/api/v1/latest?api_key=${apikey}`);
@@ -9,10 +9,10 @@ async function getDatar(poke) {
     } else {
       //converts response into json we can use
       const data = await response.json();
-      document.getElementById("api-response").textContent = data.name;
+      console.log(data);
     }
   } catch (error) {
     console.log(error);
   }
 }
-console.log("ochen da");
+convertCurrency();
